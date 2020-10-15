072617ad1104a49ffc0b26f12ef39d96bc23ad37
Walter Jr <wj@tomcix.com>
2020-10-15 20:02:55 -0300

    Exercise #2 com seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+def types
+  %w[chinese italian japanese french belgian].sample
+end
+
+puts 'Creating 20 restaurants...'
+20.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: types
+  )
+end
+puts 'Finished!'
