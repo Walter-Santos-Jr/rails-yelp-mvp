3fa05ea9cc3ad6333c9858c356c0e16853be2f11
Walter Jr <wj@tomcix.com>
2020-10-16 12:38:41 -0300

    Exercise#2 - Done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
 
-  validates :content, :rating, presence: true
-  validates :rating, :numericality => { :message => " should be a number" }
-  validates :rating, :inclusion => { :in => 0..5, :message => " should be between 0 to 5" }
+  validates :content, presence: true, length: { minimum: 10 }
+  validates :rating, inclusion: { in: 0..5 }, numericality: { only_integer: true }
 end
